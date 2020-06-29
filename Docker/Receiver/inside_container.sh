790f40813507c792cca9ecbe44c0f800cc9b8be0
smilepengg <smile.peng@mail.utoronto.ca>
2020-06-29 17:08:57 -0400

    Update inside_container.sh

--- Docker/Receiver/inside_container.sh
+++ Docker/Receiver/inside_container.sh
@@ -1,5 +1,5 @@
 #opening netcat port to listen for the bitstreamfile to transfer over
-nc -lvd 44444 > shellTop.bit & #I don't think I should have this in the background
+nc -lvd 44444 > shellTop.bit
 
 #set up vivado
 cd /opt/Xilinx/Vivado/2018.1
